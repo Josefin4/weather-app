6c2f703822ff1902ee6f248a6f6f1ced99c78b2b
Josefin4 <josefinsalom@gmail.com>
2024-12-15 15:08:24 +0100

    edit url for frontend

--- backend/demo/demo/src/main/java/com/example/demo/WebConfig.java
+++ backend/demo/demo/src/main/java/com/example/demo/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("https://amusing-imagination-production.up.railway.app/")
+                    .allowedOrigins("https://weather-app-js.up.railway.app/")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")  // Allow all headers
                     .allowCredentials(true);  // Allow credentials, if needed
