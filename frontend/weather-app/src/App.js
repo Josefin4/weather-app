80cc27a518101f8ab59677e2aaea206fad7e68eb
julia_ssaa <julia_ssaa@hotmail.com>
2024-12-29 14:32:07 +0100

    Implement tailwind, headless ui and daisy ui

--- frontend/weather-app/src/App.js
+++ frontend/weather-app/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Weather from "./Weather";
+import './index.css';
 
 const App = () => {
     return (
-        <div>
+        <div className="h-screen flex items-start justify-center pt-40 bg-gradient-to-r from-[#C6E7FF] via-[#D4F6FF] to-[#FFDDAE]">
             <Weather />
         </div>
     );
--- frontend/weather-app/src/Weather.js
+++ frontend/weather-app/src/Weather.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Button } from "@headlessui/react";
+import { Input } from '@headlessui/react'
 
 const Weather = () => {
     const [city, setCity] = useState("");
@@ -18,15 +20,18 @@ const Weather = () => {
     };
 
     return (
-        <div>
-            <h1>Weather App</h1>
-            <input
+        <div className="card bg-base-100 w-2/4 shadow-xl">
+            <div className="card-body">
+            <h1 className="card-title">Welcome!</h1>
+            <Input
+                className="input input-bordered w-full max-w-xs"
                 type="text"
                 placeholder="Enter city here"
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
             />
-            <button onClick={fetchWeather}>Get Weather</button>
+            <div className="card-actions">
+            <Button onClick={fetchWeather} className="btn btn-outline">Get Weather</Button></div>
             {weather && (
                 <div>
                     <h2>Weather in {weather.name}</h2>
@@ -34,6 +39,7 @@ const Weather = () => {
                     <p>Weather: {weather.weather[0].description}</p>
                 </div>
             )}
+        </div>
         </div>
     );
 };
